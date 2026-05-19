fa4d5621b10f3e6ec7a7a881be4aa9332f0abe97
TayyibahHq <66921146+TayyibahHq@users.noreply.github.com>
2026-05-19 16:05:43 -0500

    Routes

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,13 @@ import { createRoot } from "react-dom/client";
 import App from "./app/App.tsx";
 import "./styles/index.css";
 
+const url = new URL(window.location.href);
+const redirect = url.searchParams.get("redirect");
+
+if (redirect) {
+  window.history.replaceState({}, "", redirect);
+}
+
 createRoot(document.getElementById("root")!).render(
   <App />
 );
